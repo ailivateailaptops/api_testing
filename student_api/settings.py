fa9dbd5a790b3d7f3d8532d97c81c6a4568c2ba4
Ailivate AI Technologies <ailivateailaptops@gmail.com>
2025-03-11 13:48:44 +0530

    Beaer Token

--- student_api/settings.py
+++ student_api/settings.py
@@ -11,6 +11,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),  # Access token expires in 15 minutes
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # Refresh token expires in 7 days
+    # "ROTATE_REFRESH_TOKENS": True,  # Issue a new refresh token when refreshing access token
+    # "BLACKLIST_AFTER_ROTATION": True,  # Blacklist old refresh token when rotated
+    # "ALGORITHM": "HS256",  # JWT signing algorithm
+    # "SIGNING_KEY": SECRET_KEY,  # Use Django's secret key for signing tokens
+    # "AUTH_HEADER_TYPES": ("Bearer",),  # Authorization: Bearer <token>
+}
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'students',  # Add the app here
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
